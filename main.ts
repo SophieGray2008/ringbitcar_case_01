20e2a9ce33fff93efd605c2007684855903f7d25
SophieGray2008 <102000752+SophieGray2008@users.noreply.github.com>
2022-03-28 09:33:45 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+basic.forever(function () {
+    RingbitCar.forward()
+    basic.pause(1000)
+    RingbitCar.turnleft()
+    basic.pause(1000)
+    RingbitCar.forward()
+    basic.pause(2000)
+    RingbitCar.turnright()
+    basic.pause(1000)
+    RingbitCar.forward()
+    basic.pause(1000)
+})
